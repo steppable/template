bf58542c33a4e2919cfc82d0633941b664b8068b
Bryce Bjork <brycedbjork@gmail.com>
2025-04-24 01:30:02 -0400

    Update Tailwind CSS, change global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,10 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        // add global colors here
-      },
-      keyframes: {
-        // add global keyframes here
-      },
-      animation: {
-        // add global animations here
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
