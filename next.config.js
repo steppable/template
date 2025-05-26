1526858d093cd3f34f4c7e6f765e2572005abeb2
Bryce Bjork <brycedbjork@gmail.com>
2025-05-26 12:18:33 -0400

    Update dependencies in bun.lock

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 // @ts-check
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    // boilerplate: enable iframe embedding for website builder interface
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
